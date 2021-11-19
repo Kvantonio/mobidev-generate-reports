0c74fc362f4a87b98d5a8862026d54819099a5fc
kvantonio <kvantonio@i.ua>
2021-11-19 23:50:40 +0200

    change state report db requests

--- app/controllers/state_report.rb
+++ app/controllers/state_report.rb
@@ -4,10 +4,11 @@ require_relative File.join(File.dirname(__FILE__), '../../env.rb')
 
 class StateReport
   def call(env)
-    if !env['rack.route_params'][:state]
+    req_state = env['rack.route_params'][:state]
+    @res = {}
+    if !req_state
 
       offices = DB.exec('SELECT * FROM offices;')
-      @res = {}
       offices.each do |office|
         if @res[office["state"]]
           @res[office["state"]] << office
@@ -17,11 +18,11 @@ class StateReport
       end
 
     else
-      @res = DB.exec_params('Select * from offices where state=$1', [env['rack.route_params'][:state].upcase])
+      offices = DB.exec_params('Select * from offices where state=$1', [req_state.upcase])
 
-      return [400, { 'Content-Type' => 'text/html' }, ['<h1>No information</h1>']] unless @res.first
+      return [400, { 'Content-Type' => 'text/html' }, ['<h1>No information</h1>']] unless offices.first
 
-      @res = [@res]
+      @res[req_state.upcase] = offices
     end
     template = File.read(File.join(File.dirname(__FILE__), '../templates/states_report.erb'))
     content = ERB.new(template)
